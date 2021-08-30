7f3548f3d87b4a28dd5814b41ace9ff71538eb72
Yusup Hidayat <yusufxyz114@gmail.com>
2021-08-30 21:03:33 +0700

    koneksi

--- koneksi.php
+++ koneksi.php
@@ -1,5 +1,5 @@
 <?php
-$koneksi = mysqli_connect("localhost", "root", "", "learnify");
+$koneksi = mysqli_connect("localhost", "root", "", "elearning");
 
 // Check connection
 if (mysqli_connect_errno()) {
